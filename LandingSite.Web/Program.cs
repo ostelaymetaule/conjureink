8adbf5b97d1733d9c9e10136b29f63508ca45fb5
Alexander Elgin <alexander.elgin@hotmail.com>
2024-11-02 23:51:09 +0100

    mudblazor start

--- LandingSite.Web/Program.cs
+++ LandingSite.Web/Program.cs
@@ -1,11 +1,11 @@
 using LandingSite.Web.Components;
-
+using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddMudServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
